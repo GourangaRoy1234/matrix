3e5dc11a4e68b21bcf2821cf69d764f723ce0736
GourangaRoy1234 <mechgouranga1234@gmail.com>
2025-10-30 10:02:13 +0000

    gsap enabling

--- landing page/src/componenets/Navbar.jsx
+++ landing page/src/componenets/Navbar.jsx
@@ -1,9 +1,27 @@
 import { navLinks } from '../../constants/index.js';
+import {useGSAP } from '@gsap/react';
   const Navbar = () => {
+    useGSAP(()=>{
+      // GSAP animation code for Navbar can be added here
+      const navTween = gsap.timeline({
+        scrollTrigger: {
+          trigger: 'nav',
+          start: 'bottom top'
+      }
+    });
+    navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+        backgroundColor: '#a2707050',
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut',
+      });
+    })
+
     return (
         <nav>
             <div>
                 <a href="#home" className="flex item-center gap-2">
+                <img src="/images/logo.png" alt="logo" />
                     <p>Velvet Pour</p>
                 </a>
                 {/* Navigation Links method to create navbar*/}
